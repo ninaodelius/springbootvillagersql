1a109a92431038cca6e32529fc4b075f75359e54
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-02-28 17:35:42 +0100

    config,pagination,sorting application properties

--- cruddemo/cruddemo/src/main/java/se/nina/cruddemo/dao/VillagerRepository.java
+++ cruddemo/cruddemo/src/main/java/se/nina/cruddemo/dao/VillagerRepository.java
@@ -1,8 +1,10 @@
 package se.nina.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import se.nina.cruddemo.entity.Villager;
 
+//@RepositoryRestResource(path="residents")
 public interface VillagerRepository  extends JpaRepository <Villager, Integer> {
     //no need to write code. get CRUD methods for free.
 }
