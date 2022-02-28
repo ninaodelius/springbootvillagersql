b9b628f53a9408806ceca8a83168f1ddf0948220
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-02-28 13:30:57 +0100

    added get villager by id and add new villager to restcontroller

--- cruddemo/cruddemo/src/main/java/se/nina/cruddemo/rest/VillagerRestController.java
+++ cruddemo/cruddemo/src/main/java/se/nina/cruddemo/rest/VillagerRestController.java
@@ -2,9 +2,7 @@ package se.nina.cruddemo.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.nina.cruddemo.dao.VillagerDAO;
 import se.nina.cruddemo.entity.Villager;
 import se.nina.cruddemo.service.VillagerService;
@@ -28,6 +26,30 @@ public class VillagerRestController {
     @GetMapping("/villagers")
     public List<Villager> findAll() {
         return villagerService.findAll();
+    }
+
+    //add mapping for GET /villager/{villagerid}
+    @GetMapping("/villagers/{villagerId}")
+    public Villager getVillager(@PathVariable int villagerId) {
+
+        Villager theVillager = villagerService.findById(villagerId);
+
+        if (theVillager == null) {
+            throw new RuntimeException("Villager id not found" + villagerId);
+
+        }
+        return theVillager;
+    }
+
+    //add mapping for post /villagers - add new villager
+
+    @PostMapping("/villagers")
+    public Villager addVillager(@RequestBody Villager theVillager) {
+
+        theVillager.setId(0);
+        villagerService.save(theVillager);
+
+        return  theVillager;
     }
 
 }
